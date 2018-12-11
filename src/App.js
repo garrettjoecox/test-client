1cce19650f410d448b8418e287a7b676cda8df3a
Garrett Cox <garrettjcox@gmail.com>
2018-12-10 20:39:12 -0600

    Add fetch

--- src/App.js
+++ src/App.js
@@ -3,14 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    data: null
+  }
+
+  async refreshData() {
+    const data = await fetch('https://api.garrettcox.io');
+    const json = await data.json();
+
+    this.setState({ data: JSON.stringify(json) });
+  }
+
+  componentDidMount() {
+    this.refreshData();
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello World
-          </p>
+          {this.state.data ? (
+            <code>{this.state.data}</code>
+          ) : (
+            <h3>Loading...</h3>
+          )}
         </header>
       </div>
     );
